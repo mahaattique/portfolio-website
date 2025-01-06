dd22e8d0e7fcccbeb58061f70b9052889356e52e
Maha Attique <mattique@brynmawr.edu>
2025-01-06 11:46:33 +0500

    test 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import './styles/global.css';
+
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App'; // Adjust the path if necessary
 import './index.css'; // Adjust the path if necessary
+import './styles/global.css';
+
 
 const container = document.getElementById('root'); // Make sure 'root' exists in your index.html
 const root = createRoot(container); // Use createRoot API
